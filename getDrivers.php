36df10114cd03cec76ec1e8cf760cf774299bb7a
Lucien Lassalle <ravagnierlucas@gmail.com>
2025-04-09 13:35:16 +0200

    Add error code in getDrivers.php
    - Allowed character ó
    - Add 404 when a driver is not found
    
    Signed-off-by: Lucien Lassalle <ravagnierlucas@gmail.com>

--- getDrivers.php
+++ getDrivers.php
@@ -6,7 +6,8 @@ $driversDir = 'drivers/';
 if (isset($_GET['id'])) {
     $driverId = $_GET['id'];
 
-    if (preg_match('/[^a-zA-Z0-9_ Ááíì\'-]/', $driverId)) {
+    if (preg_match('/[^a-zA-Z0-9_ Áóáíì\'-]/', $driverId)) {
+        http_response_code(401);
         echo json_encode(["error" => "Invalid driver ID"]);
         exit;
     }
@@ -19,13 +20,15 @@ if (isset($_GET['id'])) {
             $data["picture"] = file_exists( "drivers/picture/$driverId.png") ? "drivers/picture/$driverId.png" : (file_exists( "drivers/picture/$driverId.jpg") ? "drivers/picture/$driverId.jpg" : "drivers/picture/default.png");
             echo json_encode([$data]);
         } else {
+            http_response_code(404);
             echo json_encode(["error" => "Driver not found"]);
         }
     } else {
+        http_response_code(404);
         echo json_encode(["error" => "Unauthorized access"]);
     }
     exit;
 }
-
+http_response_code(401);
 echo json_encode(["error" => "Invalid request"]);
 ?>
